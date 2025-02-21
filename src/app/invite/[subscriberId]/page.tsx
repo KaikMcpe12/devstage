b69f7ebdae5ee456b77ad097c342e8c298ad60b4
Kaik <119709479+KaikMcpe12@users.noreply.github.com>
2025-02-21 16:42:55 -0300

    :sparkles: feat: added http client

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -1,11 +1,19 @@
 import Image from 'next/image'
-import logo from '../../assets/logo.svg'
+import logo from '../../../assets/logo.svg'
+import { InviteLinkInput } from './invite-link-input'
 import { Ranking } from './ranking'
 import { Stats } from './stats'
-import { InviteLinkInput } from './invite-link-input'
 
-export default function InvitePage() {
-  const inviteLink = 'http://localhost:3000/invite/231992121'
+interface InvitePageProps {
+  params: Promise<{
+    subscriberId: string  
+  }>
+}
+
+export default async function InvitePage(props: InvitePageProps) {
+  const { subscriberId } = await props.params
+
+  const inviteLink = `http://localhost:3333/invites/${subscriberId}`
 
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
@@ -33,9 +41,9 @@ export default function InvitePage() {
             </p>
           </div>
 
-          <InviteLinkInput inviteLink={inviteLink}/>
+          <InviteLinkInput inviteLink={inviteLink} />
 
-          <Stats />
+          <Stats subscriberId={subscriberId}/>
         </div>
       </div>
 
